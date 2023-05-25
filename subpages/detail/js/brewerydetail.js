3e9f86608c45276e40bc46bb985f6b19a54191b0
geunhye-kim <mykgh6514@naver.com>
2023-05-25 17:59:37 +0900

    :sparkles: 양조장 페이지 구현
    양조장

---
+++ subpages/detail/js/brewerydetail.js
@@ -0,0 +1,70 @@
+import { injectNavbar, injectFooter } from '../../../js/navbar.js'
+
+const BACKEND_API = "http://127.0.0.1:8000";
+const FRONTEND_API = "http://127.0.0.1:5500";
+
+window.onload = async () => {
+    await injectNavbar();
+    await injectFooter();
+    BreweryCategoryShow();
+}
+
+const all = document.getElementById('all-brewery')
+const restaurant = document.getElementById('brewery-restaurant')
+const experience = document.getElementById('brewery-experience')
+
+all.onclick = () => {
+    BreweryCategoryShow()
+}
+restaurant.onclick = () => {
+    BreweryCategoryShow(restaurant)
+}
+experience.onclick = () => {
+    BreweryCategoryShow(experience)
+}
+
+async function BreweryCategoryShow(category) {
+    const response = await fetch(`${BACKEND_API}/brewery/`, {
+        method: "GET",
+    });
+
+    let response_json = await response.json()
+    response_json = response_json['results']
+
+    if (category != undefined) {
+        if (category.innerText == '식당 유무') {
+            response_json = response_json.filter(value => value.restaurant === true)
+        }
+        if (category.innerText == '체험 유무') {
+            response_json = response_json.filter(value => value.experience === true)
+        }
+    }
+
+    const brewery_category_list = document.getElementById('brewery-category-list')
+
+    brewery_category_list.innerHTML = ''
+
+    response_json.forEach(e => {
+        const id = e.id
+        const name = e.name
+        const region = e.region
+        let restaurant = 'X'
+        let experience = 'X'
+        if (e.restaurant) {
+            restaurant = 'O'
+        }
+        if (e.experience) {
+            experience = 'O'
+        }
+        
+        brewery_category_list.innerHTML += `<div class="brewery" onclick="BreweryDetailShow(${id})">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2lYEMFENrL7sVziTSqx3Nc4XyfifJnX2s4MtOldxUGw&s">
+                                    <div class="brewery-card">
+                                        <span class="brewery-name">${name}</span>
+                                        <span class="brewery-region">[${region}]</span>
+                                        <span class="brewery-res">식당 여부 : ${restaurant}</span>
+                                        <span class="brewery-exp">체험 여부 : ${experience}</span>
+                                    </div>
+                                </div>`
+    })
+}
